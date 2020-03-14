167872b6ae680af61fa94b98a21c4e2281bb6910
Youssef Restom <youssefrestom@gmail.com>
2020-03-14 19:11:25 +0000

    feat: filter parent group for items list

--- extraesia/hooks.py
+++ extraesia/hooks.py
@@ -26,8 +26,8 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_js = {"Item" : "public/js/list_view.js"}
+doctype_list_js = {"Item" : "public/js/list_view.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
---
+++ extraesia/public/js/list_view.js
@@ -0,0 +1,3 @@
+frappe.call({
+    method: "extraesia.list_view.override_get_form_params",
+});
